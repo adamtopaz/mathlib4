ebdcda53f3463985d2fe48ef95954e555fe6f269
F. G. Dorais <fgdorais@gmail.com>
2023-07-25 04:38:25 -0400

    fix: patch for std3#183

--- Mathlib/Init/Data/Bool/Lemmas.lean
+++ Mathlib/Init/Data/Bool/Lemmas.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Leonardo de Moura
 -/
 import Mathlib.Init.Data.Bool.Basic
-import Mathlib.Init.Logic
 import Mathlib.Tactic.Coe
 
 /-!
@@ -45,22 +44,14 @@ namespace Bool
 theorem cond_self.{u} {α : Type u} (b : Bool) (a : α) : cond b a a = a := by cases b <;> rfl
 #align cond_a_a Bool.cond_self
 
-@[simp]
-theorem xor_self (b : Bool) : xor b b = false := by cases b <;> simp
 #align bxor_self Bool.xor_self
 
-@[simp]
-theorem xor_true (b : Bool) : xor b true = not b := by cases b <;> simp
 #align bxor_tt Bool.xor_true
 
-theorem xor_false (b : Bool) : xor b false = b := by cases b <;> simp
 #align bxor_ff Bool.xor_false
 
-@[simp]
-theorem true_xor (b : Bool) : xor true b = not b := by cases b <;> simp
 #align tt_bxor Bool.true_xor
 
-theorem false_xor (b : Bool) : xor false b = b := by cases b <;> simp
 #align ff_bxor Bool.false_xor
 
 theorem true_eq_false_eq_False : ¬true = false := by decide
