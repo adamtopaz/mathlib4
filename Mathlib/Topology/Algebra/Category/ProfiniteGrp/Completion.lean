c345c7e80cd174c652d853b95242192b6d112518
Adam Topaz <adamtopaz@users.noreply.github.com>
2026-02-05 22:42:47 +0000

    add header

--- Mathlib/Topology/Algebra/Category/ProfiniteGrp/Completion.lean
+++ Mathlib/Topology/Algebra/Category/ProfiniteGrp/Completion.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Adam Topaz. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Adam Topaz
+-/
 module
 
 public import Mathlib.Topology.Algebra.Category.ProfiniteGrp.Limits
